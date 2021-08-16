b3375e15f5a8dcb43c30c0d922a47ec4149044fd
t-brown-designs <87334791+t-brown-designs@users.noreply.github.com>
2021-08-16 15:53:23 -0700

    java script added
    Added java script to the navigation of mobile 640px

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("Hello! Welcome to My World of Design")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
